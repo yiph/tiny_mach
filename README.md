9afc1080b1e4df9949d097e070871d319e878e34
yiph <yiphs@outlook.com>
2015-11-07 12:40:35 +0800

    machine.c almost done

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This is a tiny virtual machine (based on MIPS) as a practice for myself.
   |	0			| at		|
   |	1-2			| k0-k1		|
   |	3-4			| v0-v1		|
-  |	5-9		 	| a0-a5		|
+  |	5-9		 	| a0-a4		|
   |	10-19		| t0-t9		|
   |	20-27		| s0-s7		|
   | 28			| gp		|
@@ -42,34 +42,37 @@ This is a tiny virtual machine (based on MIPS) as a practice for myself.
 
   | NAME	  | FORMAT		| OPCODE/FUNCT(hex)	|
   | --------- |:-----------:| -----------------:|
-  | move	  | R			| 00/00				|
-  | add		  | R			| 00/01				|
+  | nop		  | R			| 00/00				|
+  | move	  | R			| 00/01				|
+  | add		  | R			| 00/02				|
   | addi	  | I			| 05				|
-  | sub		  | R			| 00/02				|
+  | sub		  | R			| 00/03				|
   | subi	  | I			| 06				|
-  | mul		  | R			| 00/03				|
-  | div		  | R			| 00/04				|
-  | mflo	  | R			| 00/05				|
-  | mfhi	  | R			| 00/06				|
-  | sll		  | R			| 00/07				|
-  | srl		  | R			| 00/08				|
-  | sra		  | R			| 00/09				|
-  | and		  | R			| 00/0a				|
+  | mul		  | R			| 00/04				|
+  | div		  | R			| 00/05				|
+  | mflo	  | R			| 00/06				|
+  | mfhi	  | R			| 00/07				|
+  | sll		  | R			| 00/08				|
+  | srl		  | R			| 00/09				|
+  | sra		  | R			| 00/0a				|
+  | and		  | R			| 00/0b				|
   | andi	  | I			| 07				|
-  | or		  | R			| 00/0b				|
+  | or		  | R			| 00/0c				|
   | ori		  | I			| 08				|
-  | not		  | R			| 00/0c				|
-  | slt		  | R			| 00/0d				|
+  | not		  | R			| 00/0d				|
+  | slt		  | R			| 00/0e				|
   | slti	  | I			| 09				|
-  | seq		  | R			| 00/0e				|
+  | seq		  | R			| 00/0f				|
   | seqi	  | I			| 0a				|
-  | lui		  | I			| 0b				|
-  | lw		  | I			| 0c				|
-  | sw		  | I			| 0d				|
-  | bze		  | I			| 0e				|
-  | bnz		  | I			| 0f				|
+  | li		  | I			| 0b				|
+  | lli		  | I			| 0c				|
+  | lui		  | I			| 0d				|
+  | lw		  | I			| 0e				|
+  | sw		  | I			| 0f				|
+  | bze		  | I			| 10				|
+  | bnz		  | I			| 11				|
   | j		  | J			| 01				|
   | jal		  | J			| 02				|
-  | jr		  | R			| 00/0f				|
+  | jr		  | R			| 00/10				|
   | syscall	  | J			| 03				|
 
--- machine.c
+++ machine.c
@@ -1,8 +1,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include "machine.h"
 
+#define FLAG(S) printf(#S"\n")
+#define PRINT(I) printf("%d\n", (I))
+
+/* Macros for generating codes about listing instructions */
 #define OP_SET_FUND(SENT)                               \
     SENT##_OP(rfmt);                                    \
     SENT##_OP(j);                                       \
@@ -14,14 +19,15 @@
     SENT##_OP(ori);                                     \
     SENT##_OP(slti);                                    \
     SENT##_OP(seqi);                                    \
+    SENT##_OP(li);                                      \
+    SENT##_OP(lli);                                     \
     SENT##_OP(lui);                                     \
     SENT##_OP(lw);                                      \
     SENT##_OP(sw);                                      \
     SENT##_OP(bze);                                     \
     SENT##_OP(bnz)
-    
-
 #define OP_SET_RFMT(SENT)                               \
+    SENT##_OP(nop);                                     \
     SENT##_OP(move);                                    \
     SENT##_OP(add);                                     \
     SENT##_OP(sub);                                     \
@@ -38,146 +44,196 @@
     SENT##_OP(slt);                                     \
     SENT##_OP(seq);                                     \
     SENT##_OP(jr)
-    
-
 #define OP_SET_ALL(SENT)                                \
     OP_SET_FUND(SENT);                                  \
     OP_SET_RFMT(SENT)
 
 #define DECLARE_FUNCT_OP(OP_NAME)                                 \
-    void OP_NAME##_f(uint32_t ins, struct machine *mach)
+    void OP_NAME##_f(uint32_t ins, MACHINE *mach)
+#define DECLARE_INS_F OP_SET_ALL(DECLARE_FUNCT)
 
 #define ASSIGN_FUND_DISPATCH_OP(OP_NAME)        \
     fund_dispatch[_##OP_NAME##_] = OP_NAME##_f
-
-#define INIT_FUND_DISPATCH                                              \
-    void (*fund_dispatch[MAX_OPCODE])(uint32_t ins, struct machine *mach); \
-    OP_SET_FUND(ASSIGN_FUND_DISPATCH)
+#define INIT_FUND_DISPATCH OP_SET_FUND(ASSIGN_FUND_DISPATCH)
 
 #define ASSIGN_RFMT_DISPATCH_OP(OP_NAME)        \
     rfmt_dispatch[_##OP_NAME##_] = OP_NAME##_f
-
-#define INIT_RFMT_DISPATCH                                              \
-    void (*rfmt_dispatch[MAX_FUNCT])(uint32_t ins, struct machine *mach); \
-    OP_SET_RFMT(ASSIGN_RFMT_DISPATCH)
-
-/* Declare all functions corresponding to instructions */
-OP_SET_ALL(DECLARE_FUNCT);
+#define INIT_RFMT_DISPATCH OP_SET_RFMT(ASSIGN_RFMT_DISPATCH)
 
 
-int run(struct machine *mach);
+DECLARE_INS_F; // Declare all functions of form INS_f
+void (*fund_dispatch[MAX_OPCODE + 1])(uint32_t ins, MACHINE *mach);
+void (*rfmt_dispatch[MAX_FUNCT + 1])(uint32_t ins, MACHINE *mach);
 
+void init_machine(MACHINE *mach);
+int run_machine(MACHINE *mach);
+
+void load_instruction_i(uint32_t ins[], int n, MACHINE * mach); // for test
 
 
 int main(void)
 {
-        //struct machine mach = malloc(sizeof(struct machine));
+    MACHINE *mach = malloc(sizeof(MACHINE));
+    init_machine(mach);
+    #if 1
+    uint32_t test_ins[1000] = {
+        IFMT_INS(li, a0, nil, 1),
+        IFMT_INS(li, a1, nil, 2),
+        RFMT_INS(add, a0, a1, a0, 0),
+        JFMT_INS(syscall, 0)
+    };
+    
+    load_instruction_i(test_ins, 1000, mach);
+    #endif
+    run_machine(mach);
     
     return 0;
 }
 
-int run(struct machine *mach)
+void load_instruction_i(uint32_t ins[], int n, MACHINE * mach)
+{
+    for (int i = 0; i < n; i++)
+        mach->memory[i] = ins[i];
+}
+
+
+int run_machine(MACHINE *mach)
 {
     INIT_FUND_DISPATCH;
     INIT_RFMT_DISPATCH;
+    uint32_t *pc_ptr = &mach->pc;
+
+    for (*pc_ptr = 0; *pc_ptr < MEMORY_SIZE; (*pc_ptr)++) {
+        uint32_t ins = mach->memory[*pc_ptr];
+        fund_dispatch[OPCODE(ins)](ins, mach);
+    }
     
     return 0;
     
 }
 
-/* Implementions of instructions */
+void init_machine(MACHINE *mach)
+{
+    mach->pc = 0;
+    mach->lo = 0;
+    mach->hi = 0;
+    int i;
+    for (i = 0; i < 32; i++)
+        mach->gpr[i] = 0;
+    for (i = 0; i < MEMORY_SIZE; i++)
+        mach->memory[i] = 0;
+}
 
-void rfmt_f(uint32_t ins, struct machine *mach)
-{}
+    
 
-void j_f(uint32_t ins, struct machine *mach)
-{}
+/* Implementations of instructions */
 
-void jal_f(uint32_t ins, struct machine *mach)
-{}
+void rfmt_f(uint32_t ins, MACHINE *mach)
+{ rfmt_dispatch[FUNCT(ins)](ins, mach); }
 
-void syscall_f(uint32_t ins, struct machine *mach)
-{}
+void j_f(uint32_t ins, MACHINE *mach)
+{ mach->pc = ADDR(ins) - 1; }
 
-void addi_f(uint32_t ins, struct machine *mach)
-{}
+void jal_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[_ra_] = mach->pc + 1; mach->pc = ADDR(ins) - 1; }
 
-void subi_f(uint32_t ins, struct machine *mach)
-{}
+void syscall_f(uint32_t ins, MACHINE *mach)
+{ printf("%d\t(0x%08x)\n", mach->gpr[_a0_], mach->gpr[_a0_]); }
 
-void andi_f(uint32_t ins, struct machine *mach)
-{}
+void addi_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] + CONST(ins); }
 
-void ori_f(uint32_t ins, struct machine *mach)
-{}
+void subi_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] - CONST(ins); }
 
-void slti_f(uint32_t ins, struct machine *mach)
-{}
+void andi_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] & CONST(ins); }
 
-void seqi_f(uint32_t ins, struct machine *mach)
-{}
+void ori_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] | CONST(ins); }
 
-void lui_f(uint32_t ins, struct machine *mach)
-{}
+void slti_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] < CONST(ins); }
 
-void lw_f(uint32_t ins, struct machine *mach)
-{}
+void seqi_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] == CONST(ins); }
 
-void sw_f(uint32_t ins, struct machine *mach)
-{}
+void li_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = CONST(ins); }
 
-void bze_f(uint32_t ins, struct machine *mach)
-{}
+void lli_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = (mach->gpr[REG0(ins)] & 0xFFFF0000) | CONST(ins); }
 
-void bnz_f(uint32_t ins, struct machine *mach)
-{}
+void lui_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = (CONST(ins) << 16) | (mach->gpr[REG0(ins)] & 0x0000FFFF); }
 
-void move_f(uint32_t ins, struct machine *mach)
-{}
+void lw_f(uint32_t ins, MACHINE *mach)
+{ mach->memory[CONST(ins) + mach->gpr[REG1(ins)]] = mach->gpr[REG0(ins)]; }
 
-void add_f(uint32_t ins, struct machine *mach)
-{}
+void sw_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->memory[CONST(ins) + mach->gpr[REG1(ins)]]; }
 
-void sub_f(uint32_t ins, struct machine *mach)
-{}
+void bze_f(uint32_t ins, MACHINE *mach)
+{ if (mach->gpr[REG0(ins)] == 0)  mach->pc = mach->pc + CONST(ins); }
 
-void mul_f(uint32_t ins, struct machine *mach)
-{}
+void bnz_f(uint32_t ins, MACHINE *mach)
+{ if (mach->gpr[REG0(ins)] != 0)  mach->pc = mach->pc + CONST(ins); }
 
-void div_f(uint32_t ins, struct machine *mach)
-{}
+// R-format
+void nop_f(uint32_t ins, MACHINE *mach)
+{ /* No operation */}
 
-void mflo_f(uint32_t ins, struct machine *mach)
-{}
+void move_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] =  mach->gpr[REG1(ins)]; }
 
-void mfhi_f(uint32_t ins, struct machine *mach)
-{}
+void add_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] + mach->gpr[REG2(ins)]; }
 
-void sll_f(uint32_t ins, struct machine *mach)
-{}
+void sub_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] - mach->gpr[REG2(ins)]; }
 
-void srl_f(uint32_t ins, struct machine *mach)
-{}
+void mul_f(uint32_t ins, MACHINE *mach)
+{
+    uint64_t product = mach->gpr[REG0(ins)] + mach->gpr[REG1(ins)];
+    mach->lo = product & 0x00000000FFFFFFFF;
+    mach->hi = product >> 32;
+}
 
-void sra_f(uint32_t ins, struct machine *mach)
-{}
+void div_f(uint32_t ins, MACHINE *mach)
+{
+    mach->lo = mach->gpr[REG0(ins)] / mach->gpr[REG1(ins)];
+    mach->hi = mach->gpr[REG0(ins)] % mach->gpr[REG1(ins)];
+}
 
-void and_f(uint32_t ins, struct machine *mach)
-{}
+void mflo_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->lo; }
 
-void or_f(uint32_t ins, struct machine *mach)
-{}
+void mfhi_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->hi; }
 
-void not_f(uint32_t ins, struct machine *mach)
-{}
+void sll_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] << SHAMT(ins); }
 
-void slt_f(uint32_t ins, struct machine *mach)
-{}
+void srl_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] >> SHAMT(ins); }
 
-void seq_f(uint32_t ins, struct machine *mach)
-{}
+void sra_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = ((int32_t) mach->gpr[REG1(ins)]) >> SHAMT(ins); }
 
-void jr_f(uint32_t ins, struct machine *mach)
-{}
+void and_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] & mach->gpr[REG2(ins)]; }
 
+void or_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] | mach->gpr[REG2(ins)]; }
 
+void not_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = ~mach->gpr[REG1(ins)]; }
+
+void slt_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] < mach->gpr[REG2(ins)]; }
+
+void seq_f(uint32_t ins, MACHINE *mach)
+{ mach->gpr[REG0(ins)] = mach->gpr[REG1(ins)] == mach->gpr[REG2(ins)]; }
+
+void jr_f(uint32_t ins, MACHINE *mach)
+{ mach->pc = mach->gpr[REG0(ins)] - 1; }
